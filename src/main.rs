4733fa7cf27d15e6c331ff1439efa511a82d54d2
John T. Wodder II <git@varonathe.org>
2024-01-25 08:47:21 -0500

    main() skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use axum::{body::Body, extract::Request, Router};
+use clap::Parser;
+use http::response::Response;
+use std::convert::Infallible;
+use std::net::IpAddr;
+use tower::service_fn;
+
+#[derive(Clone, Debug, Eq, Parser, PartialEq)]
+struct Arguments {
+    #[arg(long, default_value = "127.0.0.1")]
+    ip_addr: IpAddr,
+
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Arguments::parse();
+    let app = Router::new().nest_service("/", service_fn(handle_request));
+    let listener = tokio::net::TcpListener::bind((args.ip_addr, args.port))
+        .await
+        .context("failed to bind listener")?;
+    axum::serve(listener, app)
+        .await
+        .context("failed to serve application")?;
+    Ok(())
+}
+
+async fn handle_request(_req: Request) -> Result<Response<Body>, Infallible> {
+    todo!()
 }

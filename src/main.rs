bc3cb59023f08fb07a7407d8f3e08130c7d7204c
John T. Wodder II <git@varonathe.org>
2024-01-27 18:48:36 -0500

    Crude logging

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ use std::convert::Infallible;
 use std::net::IpAddr;
 use std::sync::Arc;
 use tower::service_fn;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::filter::LevelFilter;
 
 #[derive(Clone, Debug, Eq, Parser, PartialEq)]
 struct Arguments {
@@ -34,16 +36,21 @@ struct Arguments {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Arguments::parse();
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::TRACE)
+        .init();
     let client = Client::new(args.api_url)?;
     let templater = Templater::load()?;
     let dav = Arc::new(DandiDav::new(client, templater, args.title));
-    let app = Router::new().nest_service(
-        "/",
-        service_fn(move |req: Request| {
-            let dav = Arc::clone(&dav);
-            async move { Ok::<_, Infallible>(dav.handle_request(req).await.into_response()) }
-        }),
-    );
+    let app = Router::new()
+        .nest_service(
+            "/",
+            service_fn(move |req: Request| {
+                let dav = Arc::clone(&dav);
+                async move { Ok::<_, Infallible>(dav.handle_request(req).await.into_response()) }
+            }),
+        )
+        .layer(TraceLayer::new_for_http());
     let listener = tokio::net::TcpListener::bind((args.ip_addr, args.port))
         .await
         .context("failed to bind listener")?;

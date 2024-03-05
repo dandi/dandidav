42434c0a3149c305bbb99c021134745b7956adc2
John T. Wodder II <git@varonathe.org>
2024-03-05 15:15:32 -0500

    Fetch Git commit hash from `HEROKU_SLUG_COMMIT` if Git info not available

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use anyhow::{bail, Context};
-use std::env;
+use std::env::{var, VarError};
 use std::io::ErrorKind;
 use std::process::{Command, Stdio};
 
@@ -7,7 +7,11 @@ fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed=.git/HEAD");
     println!("cargo:rerun-if-changed=.git/refs");
     let pkg_version = getenv("CARGO_PKG_VERSION")?;
-    match get_commit_hash()? {
+    let mut commit = get_commit_hash()?;
+    if commit.is_none() {
+        commit = get_heroku_slug_commit()?;
+    }
+    match commit {
         Some(commit) => {
             println!("cargo:rustc-env=GIT_COMMIT={commit}");
             println!("cargo:rustc-env=VERSION_WITH_GIT={pkg_version} (commit: {commit})");
@@ -58,6 +62,14 @@ fn get_commit_hash() -> anyhow::Result<Option<String>> {
     }
 }
 
+fn get_heroku_slug_commit() -> anyhow::Result<Option<String>> {
+    match var("HEROKU_SLUG_COMMIT") {
+        Ok(slug) => Ok(Some(slug.chars().take(7).collect())),
+        Err(VarError::NotPresent) => Ok(None),
+        Err(VarError::NotUnicode(_)) => bail!("HEROKU_SLUG_COMMIT is not UTF-8"),
+    }
+}
+
 fn getenv(name: &str) -> anyhow::Result<String> {
-    env::var(name).with_context(|| format!("{name} envvar not set"))
+    var(name).with_context(|| format!("{name} envvar not set"))
 }
